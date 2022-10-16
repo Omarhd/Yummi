1ce045a13b2532f69d168ff2f3bac4283fc9b373
Omar <omeralhadu@gmail.com>
2022-10-16 10:46:00 +0200

    Add IBDesignables Extensions.

--- Yummi/Extensions/UIViews.swift
+++ Yummi/Extensions/UIViews.swift
@@ -8,9 +8,44 @@
 import UIKit
 
 extension UIView {
-    @IBInspectable var cornerRadius: CGFloat {
-        get { return self.cornerRadius}
-        set { self.layer.cornerRadius = newValue}
+    @IBInspectable var cornerRadius: Double {
+        get {
+            return Double(self.layer.cornerRadius)
+        }set {
+            self.layer.cornerRadius = CGFloat(newValue)
+        }
+    }
+    @IBInspectable var borderWidth: Double {
+        get {
+            return Double(self.layer.borderWidth)
+        }
+        set {
+            self.layer.borderWidth = CGFloat(newValue)
+        }
+    }
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            return UIColor(cgColor: self.layer.borderColor!)
+        }
+        set {
+            self.layer.borderColor = newValue?.cgColor
+        }
+    }
+    @IBInspectable var shadowColor: UIColor? {
+        get {
+            return UIColor(cgColor: self.layer.shadowColor!)
+        }
+        set {
+            self.layer.shadowColor = newValue?.cgColor
+        }
+    }
+    @IBInspectable var shadowOpacity: Float {
+        get {
+            return self.layer.shadowOpacity
+        }
+        set {
+            self.layer.shadowOpacity = newValue
+        }
     }
 }
 
